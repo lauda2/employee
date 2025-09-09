5a96957422ebeba819f5c5c4a3e88636f161cae5
Danny Lau <lauda2@oocl.com>
2025-09-09 15:09:33 +0800

    feat: post /employees to create Employee

---
+++ src/test/java/com/example/employee/EmployeeControllerTests.java
@@ -0,0 +1,43 @@
+package com.example.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_return_created_employee_when_post() throws Exception {
+        String requestBody = """
+                {
+                    "id": 1,
+                    "name": "John Smith",
+                    "age": 32,
+                    "gender": "Male",
+                    "salary": 5000.0
+                }""";
+        MockHttpServletRequestBuilder request = post("/employees").contentType(MediaType.APPLICATION_JSON).content(requestBody);
+
+        mockMvc.perform(request)
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").value("John Smith"))
+                .andExpect(jsonPath("$.gender").value("Male"))
+                .andExpect(jsonPath("$.age").value(32))
+                .andExpect(jsonPath("$.salary").value(5000.0));
+    }
+
+}
